658613e20ba69c39c2b41b883ce36d437b188e8c
Khalid Mehfooz <www.khalidkhan89@gmail.com>
2022-06-21 21:00:31 +0500

    New Design By Khalid Bhai

--- static/modules.js
+++ static/modules.js
@@ -3,7 +3,7 @@
 function normalTripUpdate(type) {
     var selectors;
     if (type == 0) {
-        selectors = `<select name="job-type" id="job_type">
+        selectors = `<select name="job-type" id="job_type" class="input100">
             <option value="0" selected = "selected">Please Select</option>
             <option value="1">Arrival / Departure</option>
             <option value="2">Transfer - Within City Limits</option>
@@ -12,7 +12,7 @@ function normalTripUpdate(type) {
         </select>`
     }
     else if (type == 1) {
-        selectors = `<select name="job-type" id="job_type">
+        selectors = `<select name="job-type" id="job_type" class="input100">
             <option value="0">Please Select</option>
             <option value="1" selected = "selected">Arrival / Departure</option>
             <option value="2">Transfer - Within City Limits</option>
@@ -21,7 +21,7 @@ function normalTripUpdate(type) {
         </select>`
     }
     else if (type == 2) {
-        selectors = `<select name="job-type" id="job_type">
+        selectors = `<select name="job-type" id="job_type" class="input100">
             <option value="0">Please Select</option>
             <option value="1">Arrival / Departure</option>
             <option value="2" selected = "selected">Transfer - Within City Limits</option>
@@ -30,7 +30,7 @@ function normalTripUpdate(type) {
         </select>`
     }
     else if (type == 3) {
-        selectors = `<select name="job-type" id="job_type">
+        selectors = `<select name="job-type" id="job_type" class="input100">
             <option value="0">Please Select</option>
             <option value="1">Arrival / Departure</option>
             <option value="2">Transfer - Within City Limits</option>
@@ -39,7 +39,7 @@ function normalTripUpdate(type) {
         </select>`
     }
     else if (type == 4) {
-        selectors = `<select name="job-type" id="job_type">
+        selectors = `<select name="job-type" id="job_type" class="input100">
             <option value="0">Please Select</option>
             <option value="1">Arrival / Departure</option>
             <option value="2">Transfer - Within City Limits</option>
@@ -67,14 +67,14 @@ function dailyTripsUpdate(start_date, end_date) {
         <div class="step1">
               <div class="input-group1">
                     <label class="txt2" for="start-date">Start Date <span class="red">*</span></label>
-                    <input type="date" name="start-date" id="start-date" value="${start_date}" required/>
+                    <input type="date" name="start-date" class="input100" id="start-date" value="${start_date}" required/>
               </div>
               <div class="input-group1">
                   <label class="txt2" for="end-date">End Date <span class="red">*</span></label>
-                  <input type="date" name="end-date" id="end-date" value="${end_date}" required/>
+                  <input type="date" name="end-date" class="input100" id="end-date" value="${end_date}" required/>
               </div>
               <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
+              <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
               </div>
         </div>`;
     $("#body-from").append(appendHtml)
@@ -133,15 +133,15 @@ function arrivalUpdate(meetGreet, flDetails, spRequirements) {
               </div>
               <div class="input-group1">
                 <label class="txt2" for="Name">Flight Details (E.g; SQ 123, Arr/ Dep 1800 hrs) <span class="red">*</span></label>
-                <input type="text" name="flight-details" id="flight-details" value="${flDetails}" required/>
+                <input type="text" name="flight-details"  class="input100" id="flight-details" value="${flDetails}" required/>
               </div>
               <div class="input-group1">
                 <label class="txt2" for="Name">For Special Requirements</label>
-                <input type="text" name="special-requirements" id="special-requirements" value="${spRequirements}" />
+                <input type="text" name="special-requirements"  class="input100" id="special-requirements" value="${spRequirements}" />
               </div>
               
               <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
+              <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
               </div>
             </div>`;
     $("#body-from").append(appendHtml)
@@ -150,9 +150,9 @@ function arrivalUpdate(meetGreet, flDetails, spRequirements) {
 
 function lastSubmitUpdate() {
     var appendHtml = `<div class="step3">
-              <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
-              </div>
+    <div class="btns-group text-center">
+    <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
+    </div>
             </div>`;
     $("#body-from").append(appendHtml)
     $('.step2').remove()
@@ -166,7 +166,7 @@ function normaltrip() {
     var appendHtml = `<div class="step0">
             <div class="input-group1">
                     <label class="txt2" for="job-type">Job Type <span class="red">*</span></label>
-                    <select name="job-type" id="job_type">
+                    <select name="job-type" id="job_type" class="input100">
                       <option value="0">Please Select</option>
                       <option value="1">Arrival / Departure</option>
                       <option value="2">Transfer - Within City Limits</option>
@@ -186,14 +186,14 @@ function normaltrip() {
     var appendHtml = `<div class="step1">
               <div class="input-group1">
                     <label class="txt2" for="start-date">Start Date <span class="red">*</span></label>
-                    <input type="date" name="start-date" id="start-date" required/>
+                    <input type="date" name="start-date" class="input100" id="start-date" required/>
               </div>
               <div class="input-group1">
                   <label class="txt2" for="end-date">End Date <span class="red">*</span></label>
-                  <input type="date" name="end-date" id="end-date" required/>
+                  <input type="date" name="end-date" class="input100" id="end-date" required/>
               </div>
               <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
+              <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
               </div>
             </div>`;
     $("#body-from").append(appendHtml)
@@ -228,15 +228,15 @@ function normaltrip() {
               </div>
               <div class="input-group1">
                 <label class="txt2" for="Name">Flight Details (E.g; SQ 123, Arr/ Dep 1800 hrs) <span class="red">*</span></label>
-                <input type="text" name="flight-details" id="flight-details" required/>
+                <input type="text" name="flight-details"  class="input100" id="flight-details" required/>
               </div>
               <div class="input-group1">
                 <label class="txt2" for="Name">For Special Requirements</label>
-                <input type="text" name="special-requirements" id="special-requirements" />
+                <input type="text" name="special-requirements" class="input100"  id="special-requirements" />
               </div>
               
               <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
+              <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
               </div>
             </div>`;
     $("#body-from").append(appendHtml)
@@ -245,9 +245,9 @@ function normaltrip() {
   
   function lastSubmit() {
     var appendHtml = `<div class="step3">
-              <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
-              </div>
+    <div class="btns-group text-center">
+    <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
+    </div>
             </div>`;
     $("#body-from").append(appendHtml)
     $('.step2').remove()
--- static/script-backup.js
+++ static/script-backup.js
@@ -3,8 +3,8 @@
 function normaltrip() {
     var appendHtml = `<div class="step0">
             <div class="input-group1">
-                    <label class="txt2" for="job-type">Job Type <span class="red">*</span></label>
-                    <select name="job-type" id="job_type">
+                    <label class="txt2" for="job-type" >Job Type <span class="red">*</span></label>
+                    <select name="job-type" id="job_type" class="input100">
                       <option value="0">Please Select</option>
                       <option value="1">Arrival / Departure</option>
                       <option value="2">Transfer - Within City Limits</option>
@@ -24,14 +24,14 @@ function normaltrip() {
     var appendHtml = `<div class="step1">
               <div class="input-group1">
                     <label class="txt2" for="start-date">Start Date <span class="red">*</span></label>
-                    <input type="date" name="start-date" id="start-date" required/>
+                    <input type="date" name="start-date"  class="input100" id="start-date" required/>
               </div>
               <div class="input-group1">
                   <label class="txt2" for="end-date">End Date <span class="red">*</span></label>
-                  <input type="date" name="end-date" id="end-date" required/>
+                  <input type="date" name="end-date" class="input100" id="end-date"  class="input100" required/>
               </div>
-              <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
+              <div class="btns-group text-center contact100-form-btn">
+                <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
               </div>
             </div>`;
     $("#body-from").append(appendHtml)
@@ -66,16 +66,16 @@ function normaltrip() {
               </div>
               <div class="input-group1">
                 <label class="txt2" for="Name">Flight Details (E.g; SQ 123, Arr/ Dep 1800 hrs) <span class="red">*</span></label>
-                <input type="text" name="flight-details" id="flight-details" required/>
+                <input type="text" name="flight-details"  class="input100" id="flight-details" required/>
               </div>
               <div class="input-group1">
                 <label class="txt2" for="Name">For Special Requirements</label>
-                <input type="text" name="special-requirements" id="special-requirements" />
+                <input type="text" name="special-requirements"  class="input100" id="special-requirements" />
               </div>
               
-              <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
-              </div>
+              <div class="btns-group text-center contact100-form-btn">
+              <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
+            </div>
             </div>`;
     $("#body-from").append(appendHtml)
     $('.step3').remove()
@@ -83,9 +83,9 @@ function normaltrip() {
   
   function lastSubmit() {
     var appendHtml = `<div class="step3">
-              <div class="btns-group text-center">
-                <button type="submit"  class="btn btn-next">Submit</button>
-              </div>
+    <div class="btns-group text-center contact100-form-btn">
+    <button type="submit"  class="btn-next"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="20" height="20" x="0" y="0" viewBox="0 0 520 520" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><g xmlns="http://www.w3.org/2000/svg" id="_15-Checked" data-name="15-Checked"><path d="m232.019 422.469a10 10 0 0 1 -8.624-4.938c-33.227-56.607-121.81-177.14-122.7-178.351a10 10 0 0 1 1.027-13.039l27.239-26.918a10 10 0 0 1 12.755-1.085l88.222 61.6c59.034-75.7 113.87-128 149.946-158.861 40.616-34.738 66.471-50.377 67.553-51.021a10 10 0 0 1 5.145-1.425h44.068a10 10 0 0 1 6.65 17.469c-125.143 111.464-261.232 349.13-262.592 351.518a10 10 0 0 1 -8.624 5.05z" fill="#ffffff" data-original="#000000" class=""></path><path d="m230.1 464.132c-99.252 0-180-80.747-180-180s80.748-180 180-180a179.651 179.651 0 0 1 58.491 9.72 10 10 0 1 1 -6.5 18.916 159.712 159.712 0 0 0 -52-8.636c-88.225 0-160 71.776-160 160s71.775 160 160 160 160-71.776 160-160a161.236 161.236 0 0 0 -3.246-32.231 10 10 0 1 1 19.6-4.005 181.324 181.324 0 0 1 3.651 36.236c.004 99.253-80.744 180-179.996 180z" fill="#ffffff" data-original="#000000" class=""></path></g></g></svg> Submit</button>
+  </div>
             </div>`;
     $("#body-from").append(appendHtml)
     $('.step2').remove()
